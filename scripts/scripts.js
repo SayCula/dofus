5e02d4ab4db40f61d26519f4cd7d8a6fb28638af
SayCula <98538872+SayCula@users.noreply.github.com>
2023-08-21 17:09:09 +0100

    Add files via upload

---
+++ scripts/scripts.js
@@ -0,0 +1,8 @@
+// disabled btn <a>
+
+var disabled = document.getElementById("disabled");
+
+    disabled.style.cursor = "no-drop"    
+    disabled.style.pointerEvents = "none";
+    disabled.style.opacity = "0.5";
+    
