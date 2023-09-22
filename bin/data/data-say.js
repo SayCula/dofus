00349f5a1af9d75e4ac36bea069c894382345f0f
SayCula <98538872+SayCula@users.noreply.github.com>
2023-09-22 17:46:10 +0100

    up bin

---
+++ bin/data/data-say.js
@@ -0,0 +1,13 @@
+const posts = [
+    {
+      id: "a1",
+      name: "alam",
+      img: "../imgs/icon.png",
+      data: "2023-08-17",
+      itemName: "fer",
+      quantité: 4,
+      kamas: 6000,
+      genre: ['newest products']
+    },
+
+  ]
