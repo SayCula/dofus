49f8b023ade9f0975956e6e127f33298ab95a45d
SayCula <98538872+SayCula@users.noreply.github.com>
2023-10-03 23:32:10 +0100

    Update data-rune.js

--- bin/data/data-rune.js
+++ bin/data/data-rune.js
@@ -1,7 +1,6 @@
 const Irune = document.querySelector("#itemList");
 
 Irune.innerHTML = `
-
             <input type="button" class="btn-Col" value="Clear" onclick="clearFields()">
             <input type="button" class="btn-Col" value="Undo" onclick="resetFields()">
             <input type="button" class="btn-Col" id ="plusBtn" value="+" onclick="plusInput()">
@@ -14,7 +13,10 @@ Irune.innerHTML = `
             <input class="ga" value="GaPm" type="button" onclick="addValue(90)">
             <input class="ga" value="po" type="button" onclick="addValue(51)">
             <input class="ga" value="inov" type="button" onclick="addValue(30)">
+            <input class="rune" value="So Cri" type="button" onclick="setPercentage(10)">
 
+            <input class="inexistant" type="button">
+            
             <!-- // RUNE FEU - INE -->
             
             <input class="rune" value="1" type="button" onclick="setPercentage(1)">
@@ -23,7 +25,7 @@ Irune.innerHTML = `
             
             
             <!-- // RUNE Sa -->
-          
+            
             <input class="rune" type="button" value="SA" onclick="setPercentage(3)">
             <input class="rune" type="button" value="PA SA" onclick="setPercentage(9)">
             <input class="rune" type="button" value="RA SA" onclick="setPercentage(30)">
@@ -34,12 +36,47 @@ Irune.innerHTML = `
             
             <input class="rune" type="button" value="Prospe" onclick="setPercentage(3)">
             <input class="rune" type="button" value="PA Prospe" onclick="setPercentage(9)">
+            <input class="inexistant" type="button">
             
             <!-- // RUNE Pui -->
+            <input class="rune" value="Pui" type="button" onclick="setPercentage(2)">
+            <input class="rune" value="PA Pui" type="button" onclick="setPercentage(6)">
+            <input class="rune" value="Ré Pui" type="button" onclick="setPercentage(20)">
             
-            <input class="rune" value="Ré" type="button" onclick="setPercentage(1)">
-            <input class="rune" value="PA Ré" type="button" onclick="setPercentage(3)">
-            <input class="rune" value="Ré Per" type="button" onclick="setPercentage(1)">
+            <input class="rune" value="Ré per" type="button" onclick="setPercentage(6)">
+
+            <input class="rune" value="Ré Pou - Cri" type="button" onclick="setPercentage(2)">
+            <input class="rune" value="Pa Pou - Cri" type="button" onclick="setPercentage(6)">
+            
+            <input class="rune" value="Re Pa PM" type="button" onclick="setPercentage(7)">
+            <input class="rune" value="Pa Re Pa PM" type="button" onclick="setPercentage(21)">
+            <input class="inexistant" type="button">
+
+            <input class="rune" value="Pod" type="button" onclick="setPercentage(2.5)">
+            <input class="rune" value="Pa Pod" type="button" onclick="setPercentage(7.5)">
+            <input class="rune" value="Ra Pod" type="button" onclick="setPercentage(25)">
+            
+            <input class="rune" value="Tacle - Fuite" type="button" onclick="setPercentage(4)">
+            <input class="rune" value="Pa Tacle - Fuite" type="button" onclick="setPercentage(12)">
+            <input class="inexistant" type="button">
+
+
+            <input class="rune" value="DO" type="button" onclick="setPercentage(20)">
+            <input class="rune" value="DO Per" type="button" onclick="setPercentage(15)">
+            <input class="inexistant" type="button">
+            
+            <input class="rune" value="DO Ele" type="button" onclick="setPercentage(5)">
+            <input class="rune" value="Pa DO Ele" type="button" onclick="setPercentage(15)">
+            <input class="inexistant" type="button">
+
+            
+            <input class="rune" value="Pus" type="button" onclick="setPercentage(2)">
+            <input class="rune" value="Pa Pus" type="button" onclick="setPercentage(6)">
+            <input class="rune" value="Ra Pus" type="button" onclick="setPercentage(20)">
+
+            
+
+
 
             `
 
@@ -150,4 +187,19 @@ Irune.innerHTML = `
                   }
 
                                 
-           
+           function filterItems() {
+  var input, filter, buttons, i;
+  input = document.getElementById("filterInput");
+  filter = input.value.toUpperCase();
+  buttons = document.getElementsByTagName("input");
+
+  for (i = 0; i < buttons.length; i++) {
+    if (buttons[i].type === "button") {
+      if (buttons[i].value.toUpperCase().indexOf(filter) > -1) {
+        buttons[i].style.display = "";
+      } else {
+        buttons[i].style.display = "none";
+      }
+    }
+  }
+}
