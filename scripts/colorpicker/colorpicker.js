72a40fb1174636eb5c2802aba878fc15c350b481
SayCula <98538872+SayCula@users.noreply.github.com>
2024-02-29 02:43:00 +0100

    Update colorpicker.js

--- scripts/colorpicker/colorpicker.js
+++ scripts/colorpicker/colorpicker.js
@@ -11,21 +11,7 @@
       printColor.innerHTML=`${colorPicker.value}`
       
   })
-  function hexToRGB(hex) {
-      // Remove the '#' symbol if present
-      hex = hex.replace('#', '');
-  
-      // Parse the hexadecimal values for red, green, and blue
-      const red = parseInt(hex.substring(0, 2), 16);
-      const green = parseInt(hex.substring(2, 4), 16);
-      const blue = parseInt(hex.substring(4, 6), 16);
-  
-      return { red, green, blue };
-  }
-  
-  
-  
-  // Add event listener to the input element
+
   imageInput.addEventListener('change', function (event) {
       const file = event.target.files[0];
       if (file) {
